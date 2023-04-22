4c4e6d3743b6e9c1afcdc61d6d747f55ed8724be
SinhaPrakhar38 <74342849+SinhaPrakhar38@users.noreply.github.com>
2023-04-22 11:20:12 +0530

    Add files via upload
    This file contains code for finding the kth maximum and minimum element of an array where k is taken from the user using Scanner Class and then parsing array through for loop.

---
+++ kthmaxandmin.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class kthmaxandmin{
+    public static void main(String[] args) {
+        int a[] = {1,20,30,40,50,60,70,100,90};
+        Scanner sc = new Scanner(System.in);
+        int k = sc.nextInt();
+        for (int i=0; i<a.length-1; i++){
+            for (int j = 0; j<a.length-1; j++){
+                if(a[j]>a[j+1]){
+                    int temp = a[j];
+                    a[j] = a[j+1];
+                    a[j+1] = temp;
+                }
+            }
+        }
+        System.out.println(a[k-1]);
+    }
+}
