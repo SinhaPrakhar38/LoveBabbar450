8b28cdda50ceb8175c0364195b6fc45519b0470c
SinhaPrakhar38 <74342849+SinhaPrakhar38@users.noreply.github.com>
2023-04-22 11:25:42 +0530

    Add files via upload
    This file contains code for finding the union and intersection of two arrays. The challenge was to take the 2 arrays and then check the similar elements and then finding the union and intersection in between the 2 arrays.

---
+++ UnionAndIntersectionSortedArrays.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class UnionAndIntersectionSortedArrays {
+    public static void main(String[] args) {
+        int arr1[] = {1,2,3,6,8,9,10,11};
+        int arr2[] = {3,8,5,10,11};
+        int count=0;
+        for (int i=0; i<arr1.length; i++){
+            for (int j=0; j<arr2.length; j++){
+                if (arr1[i]==arr2[j]){
+                    count++;
+                }
+            }
+        }
+        System.out.println(arr1.length+arr2.length-count);
+        System.out.println(count);
+    }    
+}
