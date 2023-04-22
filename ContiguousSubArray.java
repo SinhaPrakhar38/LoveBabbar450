c7bab62982f628902f3adb8ad8a85f84b881166d
SinhaPrakhar38 <74342849+SinhaPrakhar38@users.noreply.github.com>
2023-04-22 11:29:04 +0530

    Add files via upload
    This file contains code for finding the maximum sum from a contiguous subarray from a given array which contains positive as well as negative elements also.

---
+++ ContiguousSubArray.java
@@ -0,0 +1,19 @@
+import java.util.*; 
+
+public class ContiguousSubArray {
+ public static void main(String[] args) {
+    int arr[] = {1,2,3,4,-5,-8,9,11};
+    int min =0;
+    int add=0;
+    for (int i=0; i<arr.length; i++){
+        if(arr[i]<0){
+            min = arr[i];
+        }
+        else if(arr[i]>0){
+            add+=arr[i];
+        }
+    }
+    System.out.println(add);
+    System.out.println(min);
+ }   
+}
