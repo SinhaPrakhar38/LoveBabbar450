d50982bc6dc1fd3375d1b3ac2035979cfa100cba
SinhaPrakhar38 <74342849+SinhaPrakhar38@users.noreply.github.com>
2023-05-02 09:25:05 +0530

    Update ContiguousSubArray.java

--- ContiguousSubArray.java
+++ ContiguousSubArray.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class ContiguousSubArray {
  public static void main(String[] args) {
-    int arr[] = {1,2,3,4,-5,-8,9,11};
+    int arr[] = {1,2,3,4,-5,-8,9,20};
     int min =0;
     int add=0;
     for (int i=0; i<arr.length; i++){
