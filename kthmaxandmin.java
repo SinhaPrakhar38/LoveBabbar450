19c4bde710218b8b6f4ae9177e664d24c85295fe
SinhaPrakhar38 <74342849+SinhaPrakhar38@users.noreply.github.com>
2023-05-04 23:16:11 +0530

    Update kthmaxandmin.java

--- kthmaxandmin.java
+++ kthmaxandmin.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class kthmaxandmin{
     public static void main(String[] args) {
-        int a[] = {1,20,30,40,50,60,70,100,90};
+        int a[] = {1,20,30,40,50,60,70,100,80};
         Scanner sc = new Scanner(System.in);
         int k = sc.nextInt();
         for (int i=0; i<a.length-1; i++){
