ea12a0bde444dbe0175806459aa9b806c31aba03
SinhaPrakhar38 <74342849+SinhaPrakhar38@users.noreply.github.com>
2023-05-07 09:19:36 +0530

    Added Negative number to make it challenging.

--- UnionAndIntersectionSortedArrays.java
+++ UnionAndIntersectionSortedArrays.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class UnionAndIntersectionSortedArrays {
     public static void main(String[] args) {
         int arr1[] = {1,2,3,6,8,9,10,11};
-        int arr2[] = {3,8,5,10,11};
+        int arr2[] = {3,8,5,10,-11};
         int count=0;
         for (int i=0; i<arr1.length; i++){
             for (int j=0; j<arr2.length; j++){
